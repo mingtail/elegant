71060494c4ca1c9036b4ef98e1d3f85babec7139
mingtail <mingtail@qq.com>
2017-11-22 15:02:27 +0800

    modify config

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -8,6 +8,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
 
+process.env.HOST = '0.0.0.0'
+
 const devWebpackConfig = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
