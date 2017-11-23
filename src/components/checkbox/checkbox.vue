55c5874adf40de4c26b1f98eedaaa6a86a446ea6
mingtail <mingtail@qq.com>
2017-11-23 14:01:39 +0800

    add

--- src/components/checkbox/checkbox.vue
+++ src/components/checkbox/checkbox.vue
@@ -4,7 +4,7 @@
     <ul class="check-list">
       <li v-for="(item, index) in list" class="check-item border-bottom-1px">
         <input :id="`item_${index}`" type="checkbox" class="toggle" v-model="item.isChecked">
-        <label :for="`item_${index}`">{{item.content}}</label>
+        <label :for="`item_${index}`" class="text" :class="{checked: item.isChecked}">{{item.content}}</label>
       </li>
     </ul>
   </div>
@@ -23,8 +23,6 @@
           {content: '打豆豆', isChecked: false}
         ]
       }
-    },
-    computed: {
     },
     components: {
       IHeader
@@ -48,20 +46,32 @@
       .check-item
         position relative
         display flex
-        padding 15px 20px
+        padding 5px 20px
+        transition all .2s
+        &:active
+          background-color $color-light-grey-sss
         .toggle
-          /*display none*/
-          text-align: center;
-          width: 40px;
-          /* auto, since non-WebKit browsers doesn't support input styling */
-          height: auto;
-          position: absolute;
-          top: 5px;
-          bottom: 0;
-          margin: auto 0;
-          appearance: none;
-          &:after {
-            content url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="-10 -18 100 135"><circle cx="50" cy="50" r="40" fill="none" stroke="#ededed" stroke-width="3"/></svg>')
-          }
-
+          position relative
+          flex 0 0 40px
+          width 40px
+          text-align center
+          appearance none
+        .text
+          flex 1
+          padding-left 20px
+          align-self center
+          transition all .4s
+          &.checked
+            color $color-light-grey
+            text-decoration line-through
+</style>
+<style scoped rel="css/text">
+  .toggle::after{
+    display: inline-block;
+    content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="-10 -18 100 135"><circle cx="50" cy="50" r="40" fill="none" stroke="#ededed" stroke-width="3"/></svg>');
+  }
+  .toggle:checked::after{
+    display: inline-block;
+    content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="-10 -18 100 135"><circle cx="50" cy="50" r="40" fill="none" stroke="#bddad5" stroke-width="3"/><path fill="#5dc2af" d="M72 25 L42 71 27 56 l-4 4 20 20 34-52 Z"/></svg>');
+  }
 </style>
