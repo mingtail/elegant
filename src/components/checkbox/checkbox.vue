8dc4b40bf155325f213002e59d32d666023fd4cd
mingtail <mingtail@qq.com>
2017-11-24 13:50:10 +0800

    add

--- src/components/checkbox/checkbox.vue
+++ src/components/checkbox/checkbox.vue
@@ -71,7 +71,6 @@
     content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="-10 -18 100 135"><circle cx="50" cy="50" r="40" fill="none" stroke="#ededed" stroke-width="3"/></svg>');
   }
   .toggle:checked::after{
-    display: inline-block;
     content: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="-10 -18 100 135"><circle cx="50" cy="50" r="40" fill="none" stroke="#bddad5" stroke-width="3"/><path fill="#5dc2af" d="M72 25 L42 71 27 56 l-4 4 20 20 34-52 Z"/></svg>');
   }
 </style>
