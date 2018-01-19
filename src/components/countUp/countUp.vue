3a53a2dcce2d3b46733633fd93a8d399198a944d
mingtail <mingtail@qq.com>
2018-01-19 16:34:51 +0800

    add count

---
+++ src/components/countUp/countUp.vue
@@ -0,0 +1,58 @@
+<template>
+  <span>{{startVal}}</span>
+</template>
+<script>
+  import CountUp from 'countup.js'
+
+  export default {
+    name: 'count-up',
+    mounted () {
+      this.$nextTick(() => {
+        this._countup = new CountUp(this.$el, this.startVal, this.endVal, this.decimals, this.duration, this.options)
+        if (this.start) {
+          this._countup.start()
+        }
+      })
+    },
+    props: {
+      start: {
+        type: Boolean,
+        default: true
+      },
+      startVal: {
+        type: Number,
+        default: 0
+      },
+      endVal: {
+        type: Number,
+        required: true
+      },
+      // number of decimal places in number
+      decimals: {
+        type: Number,
+        default: 0
+      },
+      // duration in seconds
+      duration: {
+        type: Number,
+        default: 2
+      },
+      options: {
+        type: Object,
+        default () {
+          return {}
+        }
+      }
+    },
+    watch: {
+      start (val) {
+        if (val) {
+          this._countup.start()
+        }
+      },
+      endVal (val) {
+        this._countup.update(val)
+      }
+    }
+  }
+</script>
---
+++ src/pages/count.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="count-wrap">
+    <i-header title="Count"></i-header>
+    <count-up :start-val="1000" :end-val="100" :duration="1"></count-up>
+
+    <div class="btn-wrap">
+      <btn text="开始" @click.native="startCount"></btn>
+    </div>
+  </div>
+</template>
+
+<script>
+  import IHeader from '../components/header/iheader'
+  import CountUp from '../components/countUp/countUp'
+  import Btn from '../components/btn/btn'
+
+  export default {
+    name: "count",
+    methods: {
+      startCount() {
+        console.log(1)
+      }
+    },
+    components: {
+      IHeader,
+      CountUp,
+      Btn
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+  .count-wrap
+    position fixed
+    left 0
+    top 0
+    width 100%
+    height 100%
+    overflow-y auto
+    background-color #fff
+    .btn-wrap
+      margin-top 30px
+      text-align center
+</style>
