7c2a2ecbc36dfc39913248876b7d070fad8809cc
mingtail <mingtail@qq.com>
2019-06-24 16:37:22 +0800

    update countup.js v2.0.4

--- src/components/countUp/countUp.vue
+++ src/components/countUp/countUp.vue
@@ -1,72 +1,89 @@
 <template>
-  <span>{{startVal}}</span>
+  <span/>
 </template>
-<script>
-  import CountUp from 'countup.js'
 
+<script>
+  import { CountUp } from 'countup.js';
+  const typeOf = type => object => Object.prototype.toString.call(object) === `[object ${type}]`;
+  const isFunction = typeOf('Function');
   export default {
+    __countup__: CountUp,
     name: 'count-up',
-    mounted () {
-      this.$nextTick(() => {
-        this._countup = new CountUp(this.$el, this.startVal, this.endVal, this.decimals, this.duration, this.options)
-        if (this.start) {
-          this._countup.start()
-        }
-      })
-    },
-    methods: {
-      startCount() {
-        this._countup.start()
-      },
-      reset() {
-        this._countup.reset()
-      },
-      pauseResume() {
-        this._countup.pauseResume()
-      },
-      // 直接调用update函数更新。 也可以直接更新数据, 通过watch开触发update函数
-      update(num) {
-        this._countup.update(num)
-      }
-    },
     props: {
-      start: {
-        type: Boolean,
-        default: true
-      },
-      startVal: {
+      delay: {
         type: Number,
-        default: 0
+        required: false,
+        default: 0,
       },
       endVal: {
         type: Number,
-        required: true
-      },
-      // number of decimal places in number
-      decimals: {
-        type: Number,
-        default: 0
-      },
-      // duration in seconds
-      duration: {
-        type: Number,
-        default: 2
+        required: true,
       },
       options: {
         type: Object,
-        default () {
-          return {}
-        }
-      }
+        required: false,
+      },
+    },
+    data() {
+      return {
+        instance: null,
+      };
+    },
+    mounted() {
+      this.create();
     },
     watch: {
-      start (val) {
-        if (val) {
-          this._countup.start()
+      endVal: {
+        handler(value) {
+          if (this.instance && isFunction(this.instance.update)) {
+            this.instance.update(value);
+          }
+        },
+        deep: false
+      }
+    },
+    beforeDestroy() {
+      this.destroy();
+    },
+    methods: {
+      create() {
+        const that = this;
+        if (that.instance) {
+          return;
+        }
+        that.instance = new CountUp(that.$el, that.endVal, that.options);
+        if (that.instance.error) {
+          // error
+          return;
+        }
+        if (that.delay < 0) {
+          that.start();
+          return;
+        }
+        setTimeout(() => that.start(), that.delay);
+      },
+      destroy() {
+        this.instance = null;
+      },
+      start(callback) {
+        if (this.instance && isFunction(this.instance.start)) {
+          return this.instance.start(callback);
         }
       },
-      endVal (val) {
-        this._countup.update(val)
+      pauseResume() {
+        if (this.instance && isFunction(this.instance.pauseResume)) {
+          return this.instance.pauseResume();
+        }
+      },
+      reset() {
+        if (this.instance && isFunction(this.instance.reset)) {
+          return this.instance.reset();
+        }
+      },
+      update(newEndVal) {
+        if (this.instance && isFunction(this.instance.update)) {
+          return this.instance.update(newEndVal);
+        }
       }
     }
   }
