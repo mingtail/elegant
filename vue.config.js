e362330caeca39727815c47456e7e28a4f56c053
mingtail <mingtail@qq.com>
2020-07-18 00:23:45 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,30 @@
 // vue.config.js
-const productionMode = process.env.NODE_ENV === 'production';
+const debug = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   devServer: {
-    host: '0.0.0.0',
     port: 2552,
   },
   publicPath: './',
   outputDir: 'dist',
-  productionSourceMap: false,
-  css: {
-    extract: productionMode,
-    sourceMap: productionMode,
-  },
+  productionSourceMap: debug,
+  
   chainWebpack: (config) => {
 
     // 修改images打包配置
-    config.module.rule('images')
+    config.module
+      .rule('images')
       .use('url-loader')
       .loader('url-loader')
-      .tap((options) => {
-        options.limit = 10000;
-        options.name = 'image/[name].[hash:8].[ext]';
-        options.fallback.options.name = 'image/[name].[hash:8].[ext]';
-        return options;
-      });
-  },
+      .tap((options) => Object.assign(options, { limit: 10240 }))
+
+    // terser插件: 移除console
+    if (!debug) {
+      config.optimization.minimizer('terser').tap((args) => {
+        args[0].terserOptions.compress.drop_console = true
+        return args
+      })
+    }
+  }
 
 }
